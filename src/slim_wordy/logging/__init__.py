920fcb5db8af2dec05699ad5bc30a45aca5a816a
typ1cal <yashwadhawe@gmail.com>
2024-11-18 18:43:01 -0500

    fixed logger file, tried logger for the first time

--- src/slim_wordy/logging/__init__.py
+++ src/slim_wordy/logging/__init__.py
@@ -1,23 +1,19 @@
-# Empty file
-
 import os
 import sys
 import logging
 
-logging_str = "[%(asctime)s: %(levelname)s: %(modeule)s: %(message)s]"
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 log_dir = "logs"
 log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
-
 logging.basicConfig(
-    Level= logging.INFO,
+    level=logging.INFO,
     format=logging_str,
-
     handlers=[
         logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout)  # Output to console as well as log file
+        logging.StreamHandler(sys.stdout)
     ]
 )
 
-logger = logging.getLogger("de-eminemllogger")
+logger = logging.getLogger("de-eminemlogger")
